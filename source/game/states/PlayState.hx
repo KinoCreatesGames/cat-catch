760818faf5732b76b013518d1040650338753639
Kino  Rose <kinoarose@gmail.com>
2021-09-05 10:16:53 -0400

    Add in check for the level 0

--- source/game/states/PlayState.hx
+++ source/game/states/PlayState.hx
@@ -3,10 +3,10 @@ package game.states;
 import flixel.text.FlxText;
 import flixel.FlxState;
 
-class PlayState extends FlxState {
+class PlayState extends BaseGameState {
 	override public function create() {
 		super.create();
-		add(new FlxText("Hello World", 32).screenCenter());
+		createLevel(project.all_levels.Level_0);
 	}
 
 	override public function update(elapsed:Float) {
